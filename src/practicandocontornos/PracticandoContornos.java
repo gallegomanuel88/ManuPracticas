922f23abcf2c25f7001895f9088ff0837038600a
galle <galle@192.168.0.14>
2016-05-08 20:12:28 +0200

    Primer commit

---
+++ src/practicandocontornos/PracticandoContornos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicandocontornos;
+
+/**
+ *
+ * @author galle
+ */
+public class PracticandoContornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hola me llamo manuel
+    }
+    
+}
